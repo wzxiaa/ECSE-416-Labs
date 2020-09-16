406f1d7aea415956f70d5cac10896e6c1c693d74
Wenzong Xia <wenzong.xia@mail.mcgill.ca>
2020-09-16 04:16:48 -0400

    full implementation of server and client

--- Lab1/client.py
+++ Lab1/client.py
@@ -1,14 +1,8 @@
 import socket
 import sys
-import base64
-from PIL import Image
-import io
 import pickle
-
-from io import BytesIO
+import math
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
 
 def parser():
     argv = sys.argv[1:]
@@ -20,43 +14,59 @@ def parser():
     filename = argv[2]
     return servername, serverport, filename
 
+class Client():
+    def __init__(self, headerSize=64, bufferSize=4096):
+        self.headerSize = headerSize
+        self.bufferSize = bufferSize
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, serverName, serverPort):
+        try:
+            self.clientSocket.connect((serverName, serverPort))
+        except socket.error as msg:
+            print("Connection: FAILED")
+            exit(1)
+        print("Connection: OK")
+
+    def send(self, msg):
+        message = msg.encode()
+        msg_length = len(message)
+        send_length = str(msg_length).encode()
+        send_length += b' ' * (self.headerSize - len(send_length))
+        self.clientSocket.send(send_length)
+        self.clientSocket.send(message)
+        print("Request message sent.")
+
+    def get_response(self):
+        msg_length = self.clientSocket.recv(self.headerSize).decode()
+        if msg_length:
+            msg_length = int(msg_length)
+            n_recvs = math.ceil(msg_length / self.bufferSize)
+            full_response = []
+            for i in range(n_recvs):
+                msg = self.clientSocket.recv(self.bufferSize)
+                full_response.append(msg)
+            full_response = pickle.loads(b"".join(full_response))
+            print(full_response["header"])
+            if full_response["status"] == 200:
+                if full_response["ext"] == "txt":
+                    print(full_response["response"])
+                elif full_response["ext"] == "jpg":
+                    plt.imshow(full_response["response"])
+                    plt.show()
+            else:
+                print(full_response["response"])
+
+    def close(self):
+        self.clientSocket.close()
+        print("Socket closed.")
 
 if __name__ == "__main__":
-    # get the list of arguments except for the filename
+
     serverName, serverPort, filename = parser()
-    # print(("{},{},{}").format(serverName, serverPort, filename))
+    client = Client()
+    client.connect(serverName, serverPort)
+    client.send(filename)
+    client.get_response()
+    client.close()
 
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        clientSocket.connect((serverName, serverPort))
-    except socket.error as msg:
-        print("Connection: FAILED")
-        exit(1)
-
-    print("Connection: OK")
-
-    # Send data to the socket, returns the number of bytes sent. Message integrity has to
-    # be checked by the application itself
-    try:
-        clientSocket.send(filename.encode())
-    except socket.error as msg:
-        print("Connection: FAILED")
-        exit(1)
-
-    print("Request message sent.")
-
-    data = b""
-    while True:
-        buf = clientSocket.recv(4096)
-        if not buf: break
-
-        data += buf
-        if filename.endswith('.txt'):
-            print("from client", data.decode())
-        elif filename.endswith('.jpg'):
-            image_result = open('decode.jpg', 'wb')
-            image_result = pickle.loads(data)
-
-            plt.imshow(mpimg.imread('decode.jpg'))
-
-    clientSocket.close()
--- Lab1/server.py
+++ Lab1/server.py
@@ -1,60 +1,104 @@
 import socket
-import requests
-import base64
-import sys
-import pickle
-
 from PIL import Image
-serverPort = 12345
-serverName = 'localhost'
+import pickle
+import numpy as np
+import math
+from os import path
 
-# a pair (host, port) is used for the AF_INET (IPv4) address family
-# SOCK_STREAM: socket type
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind((serverName, serverPort))
+class Server():
+    def __init__(self, headerSize=64, bufferSize=4096):
+        self.headerSize = headerSize
+        self.bufferSize = bufferSize
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# socket.listen method enables a server to accept connections.
-# It specifies the number of unaccepted connections that the system will allow
-# before refusing new connections
-serverSocket.listen(1)
-print('The server is ready to receive')
-print('The server socket is listening on port: ', serverSocket.getsockname())
+    def bind(self, serverName, serverPort):
+        self.serverSocket.bind((serverName, serverPort))
 
-while True:
-    # socket.accept(): accept a connection; conn is a new socket object usable to send and receive data
-    # addr is the address bounded to the socket on the other end of the connection
-    connectionSocket, addr = serverSocket.accept()
-    print('Connected by: ', addr)
-    # Receive data from socket. Returns a bytes object representing the data. Need to specifies buffer size in bytes.
-    # request = ''
-    # counter = 0
-    # while True:
-    #     buffer = connectionSocket.recv(4096)
-    #     # print(buffer)
-    #     if not buffer:
-    #         print("ssssssssssss")
-    #         break
-    #     request += buffer.decode()
-    #     # print(buffer.decode())
-    #     print(counter)
-    #     counter = counter + 1
-    # print(request)
-    request = connectionSocket.recv(1024).decode();
-    # print(request)
+    def start(self):
+        while True:
+            # socket.accept(): accept a connection; conn is a new socket object usable to send and receive data
+            # addr is the address bounded to the socket on the other end of the connection
+            self.serverSocket.listen(1)
+            connectionSocket, addr = self.serverSocket.accept()
+            self.handle_client(connectionSocket)
+            connectionSocket.close()
 
-    try:
-        if request.endswith('.txt'):
-            f = open(request,"r")
-        elif request.endswith('.jpg'):
-            ig = pickle.dumps(open(request,"rb").read())
+    def handle_client(self, conn):
+        msg_length = self.get_Header(conn)
+        n_recvs = math.ceil(msg_length / self.bufferSize)
+        full_request = []
+        for i in range(n_recvs):
+            msg = conn.recv(self.bufferSize).decode()
+            full_request.append(msg)
+        full_request = ''.join(full_request)
 
-    except OSError as e:
-        print("404 Not Found")
+        self.send_response(conn, full_request)
 
-    if request.endswith('.txt'):
-        response = f.read()
-    if request.endswith('.txt'):
-        connectionSocket.send(response.encode())
-    elif request.endswith('.jpg'):
-        connectionSocket.send(ig)
-    connectionSocket.close()
+    def get_Header(self, conn):
+        msg_length = conn.recv(self.headerSize).decode()
+        if msg_length:
+            msg_length = int(msg_length)
+        return msg_length
+#
+    def send_response(self, conn, file_name):
+        full_response = {}
+        if path.exists(file_name):
+            http_status = 200
+            full_response["status"] = http_status
+            http_status_str = str(http_status) + " OK"
+            if file_name.endswith(".txt"):
+                file_extension = "txt"
+                full_response["ext"] = file_extension
+                http_header = ("Server HTTP Response: HTTP/1.1 {}\nContent-type: {}/html\n\n").format(http_status_str, file_extension)
+                file = open(file_name, mode='r')
+                file_content = file.read()
+                full_response["header"] = http_header
+                full_response["response"] = file_content
+                pkl_response = pickle.dumps(full_response)
+                response_length = len(pkl_response)
+                send_length = str(response_length).encode()
+                send_length += b' '*(self.headerSize - len(send_length))
+                conn.send(send_length)
+                conn.send(pkl_response)
+
+            elif file_name.endswith(".jpg"):
+                file_extension = "jpg"
+                full_response["ext"] = file_extension
+                http_header = ("Server HTTP Response: HTTP/1.1 {}\nContent-type: {}/image\n\n").format(http_status_str, file_extension)
+                img = Image.open(file_name)
+                img_arr = np.asarray(img)
+                full_response["header"] = http_header
+                full_response["response"] = img_arr
+                pkl_response = pickle.dumps(full_response)
+                response_length = len(pkl_response)
+                send_length = str(response_length).encode()
+                send_length += b' '*(self.headerSize - len(send_length))
+                conn.send(send_length)
+                conn.send(pkl_response)
+        else:
+            http_status = 404
+            full_response["status"] = http_status
+            full_response["ext"] = "None"
+            http_status_str = str(http_status) + " not found"
+            http_header = ("Server HTTP Response: HTTP/1.1 {}\n\n").format(http_status_str)
+            response = "404 Not Found"
+            full_response["header"] = http_header
+            full_response["response"] = response
+            pkl_response = pickle.dumps(full_response)
+            response_length = len(pkl_response)
+            send_length = str(response_length).encode()
+            send_length += b' ' * (self.headerSize - len(send_length))
+            conn.send(send_length)
+            conn.send(pkl_response)
+
+
+if __name__ == "__main__":
+
+    serverPort = 12346
+    serverName = 'localhost'
+
+    server = Server()
+    server.bind(serverName, serverPort)
+    server.start()
+
+
